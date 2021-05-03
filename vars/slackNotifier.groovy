791ccea920d26608e660f5d78ced51e064bdda42
vaibhav <vaibhav@vaibhavs-MacBook-Pro-2.local>
2021-05-03 14:50:43 +0530

    add config map values

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,16 @@
 def call(String buildResult) {
     
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "#3ED421", message: "Job: ` ${env.JOB_NAME} ` with buildnumber ` ${env.BUILD_NUMBER} ` was successful @team_qa"
+    slackSend color: "#3ED421", message: "Job:  with buildnumber  was successful @team_qa"
   }
   else if( buildResult == "FAILURE" ) {
-    slackSend color: "#F92F03", message: "Job: ` ${env.JOB_NAME} ` with buildnumber ` ${env.BUILD_NUMBER} ` was failed @team_devops @team_qa"
+    slackSend color: "#F92F03", message: "Job:  with buildnumber  was failed @team_devops @team_qa"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "#FE9722", message: "Job: ` ${env.JOB_NAME} ` with buildnumber ` ${env.BUILD_NUMBER} ` was unstable @team_devops @team_qa"
+    slackSend color: "#FE9722", message: "Job:  with buildnumber  was unstable @team_devops @team_qa"
   }
   else {
-    slackSend color: "#2222FE", message: "Job: ` ${env.JOB_NAME} ` with buildnumber ` ${env.BUILD_NUMBER} ` its result was unclear @team_devops @team_qa" 
+    slackSend color: "#2222FE", message: "Job:  with buildnumber  its result was unclear @team_devops @team_qa" 
   }
   
 }
